440778834e4da5c4e5945316817f1cda1a8f0d1e
dnkrit <dnkrit@yandex.ru>
2024-04-25 23:33:05 +0300

    homework

--- main.py
+++ main.py
@@ -1,13 +1,1 @@
-
 print('я конфликтую')
-
-for i in range(8):
-    print('слово')
-a, b = 9, 8
-if a > b:
-    print('a больше b')
-elif b > a:
-    print('b больше a')
-else:
-    print('a равно b')
-
